15e2c81a9371aab0f80f17434205d9caa4212c7a
Matthew Shin <tshin3232@gmail.com>
2020-06-17 00:26:45 -0400

    Update EAMCS video, testing lossless compressed gallery photo

--- src/assets/img/gallery/EASStudio/studioPhotos.js
+++ src/assets/img/gallery/EASStudio/studioPhotos.js
@@ -1,4 +1,4 @@
-import ducksRiver from "assets/img/gallery/EASStudio/ducks_river.jpg";
+import ducksRiver from "assets/img/gallery/EASStudio/ducks_river_compressed_lossless.jpg";
 
 export const studioPhotos = [
   {
--- src/views/EAMCSPage/Sections/EAMCSAboutSection.js
+++ src/views/EAMCSPage/Sections/EAMCSAboutSection.js
@@ -37,7 +37,7 @@ export default function EAMCSAboutSection() {
       <GridContainer justify="center">
         <GridItem xs={12} sm={12} md={8}>
           <VideoPlayer
-            url="https://www.facebook.com/EvergreenMath/videos/3115002501878985/"
+            url="https://www.facebook.com/EvergreenMath/videos/272982913810686/"
             width="100%"
             height="100%"
             controls={true}
