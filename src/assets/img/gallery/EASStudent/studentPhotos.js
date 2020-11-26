0adbc334d51e338e95821e9b1f8e20da4323c49f
Matthew Shin <tshin3232@gmail.com>
2020-11-26 16:30:03 -0500

    Sizing adjustments in student gallery.

--- src/assets/img/gallery/EASStudent/studentPhotos.js
+++ src/assets/img/gallery/EASStudent/studentPhotos.js
@@ -1,14 +1,14 @@
 export const studentPhotos = [
   {
     src: "https://lh3.google.com/u/0/d/1S0jd9EBjjRo0gCWh0P1KdAosvSIVbcGi=w958-h911-iv1",
-    width: 1,
-    height: 1,
+    width: 4,
+    height: 3,
     title: "Circle A"
   },
   {
     src: "https://lh3.google.com/u/0/d/1eGyFW7ls_drcMBCQ5XGDkwOAfR7QJb26=w958-h911-iv1",
-    width: 1,
-    height: 1,
+    width: 4,
+    height: 3,
     title: "Own Charcoal 12x16 Paper"
   },
 
